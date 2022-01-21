09e5cd7b40a42896bc2e9a8f5bb4db959a12101d
djavagit <58994972+djavagit@users.noreply.github.com>
2022-01-21 10:25:34 +0100

    ca marche

---
+++ main.js
@@ -0,0 +1,32 @@
+var soap = require('soap');
+var express = require('express');
+var app = express();
+var router = express.Router();
+var url = 'http://localhost:8000/?wsdl';
+var args = {};
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+const promise = new Promise((resolve, reject) => {
+    soap.createClient(url, function (err, client) {
+        client.all_vehicule(args, function (err, result) {
+            resolve(result["all_vehiculeResult"]["data"]);
+        })
+    })
+})
+promise.then((value) => {
+    console.log(value)
+    router.get('/', function (req, res) {
+        res.render('index', {
+            vehicules: value
+        })
+    })
+})
+
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
