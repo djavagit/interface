59f104af531a579e44cc6d0dbfcff49efdd28e7b
djavagit <58994972+djavagit@users.noreply.github.com>
2022-02-03 15:26:38 +0100

    websockets

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
-var soap = require('soap');
-var express = require('express');
-var app = express();
-var router = express.Router();
+const soap = require('soap');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const path = require('path');
+const io = new Server(server);
 var url = 'https://vehicules-soap.herokuapp.com/?wsdl';
 var args = {};
+let vehicules = [];
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-const promise = new Promise((resolve, reject) => {
-    soap.createClient(url, function (err, client) {
-        client.all_vehicule(args, function (err, result) {
-            resolve(result["all_vehiculeResult"]["data"]);
-        })
-    })
-})
-promise.then((value) => {
-    router.get('/', function (req, res) {
-        res.render('index', {
-            vehicules: value
-        })
+soap.createClient(url, function (err, client) {
+    client.all_vehicule(args, function (err, result) {
+        vehicules = result["all_vehiculeResult"]["data"];
     })
 })
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
 
-//add the router
-app.use('/', router);
-app.listen(process.env.PORT || 3000);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    io.emit('vehicules', vehicules);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+
+})
+
+// const promise = new Promise((resolve, reject) => {
+//     soap.createClient(url, function (err, client) {
+//         client.all_vehicule(args, function (err, result) {
+//             resolve(result["all_vehiculeResult"]["data"]);
+//         })
+//     })
+// })
+// promise.then((value) => {
+//     router.get('/', function (req, res) {
+//         res.render('index', {
+//             vehicules: value
+//         })
+//     })
+// })
+
+
+
+server.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
