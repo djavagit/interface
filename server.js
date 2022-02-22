188d24c37539ff55e98b3201a590b5b30d28f350
willygentet <58994972+willygentet@users.noreply.github.com>
2022-02-22 13:08:03 +0100

    map

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const path = require('path');
 const axios = require('axios');
-const { emit } = require('process');
+const { response } = require('express');
 const io = new Server(server);
 var url = 'https://vehicules-soap.herokuapp.com/?wsdl';
 var args = {};
@@ -18,6 +18,7 @@ soap.createClient(url, function (err, client) {
     })
 })
 
+app.use(express.static('public'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
@@ -36,6 +37,17 @@ io.on('connection', (socket) => {
             .then(response => {
                 time = response.data.res;
                 io.emit('displayTime', Math.floor(time / 60), time % 60)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    })
+
+    socket.on('callTraceRoute', () => {
+        axios.get('https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624835290f82bf4643548cfbf31e939c3b43&start=8.681495,49.41461&end=8.687872,49.420318')
+            .then(response => {
+                console.log(response.data.features[0].geometry.coordinates);
+                io.emit('traceRoute', response.data.features[0].geometry.coordinates);
             })
             .catch(error => {
                 console.log(error);
