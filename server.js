f43b78e811abe95b733bc7a6b1bb04172b40b8c4
willygentet <58994972+willygentet@users.noreply.github.com>
2022-03-01 01:34:46 +0100

    debut bornes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var args = {};
 let vehicules = [];
 let start_lon, start_lat, end_lon, end_lat;
 
-function switchLonLat(tab){
+function switchLonLat(tab) {
     for (let i = 0; i < tab.length; i++) {
         let tmp = tab[i][0];
         tab[i][0] = tab[i][1];
@@ -62,52 +62,37 @@ io.on('connection', (socket) => {
             start_lat = responses[0].data.features[0].properties.lat;
             end_lon = responses[1].data.features[0].properties.lon;
             end_lat = responses[1].data.features[0].properties.lat;
-            console.log('recup')
+            console.log(start_lon + ' ' + start_lat + ' ' + end_lon + ' ' + end_lat);
         })).catch(errors => {
             console.log(errors);
         })
-        .finally(() => {
-            console.log(end_lon);
-            axios.get('https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624835290f82bf4643548cfbf31e939c3b43&start=' + start_lon + ',' + start_lat + '&end=' + end_lon + ',' + end_lat)
-                .then(response => {
-                    let res = switchLonLat(response.data.features[0].geometry.coordinates);
-                    console.log(res);   
-                    io.emit('traceRoute', res, start_lon, start_lat);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        })
-        // axios.get('https://api.geoapify.com/v1/geocode/search?text=' + start.split(' ').join('%20') + '&apiKey=decf2dfaae66445faf7b1b318e8b1180')
-        //     .then(response => {
-        //         start_lon = response.data.features[0].lon;
-        //         start_lat = response.data.features[0].lat;
-        //     })
-        //     .catch(error => {
-        //         console.log(error);
-        //     });
-        // axios.get('https://api.geoapify.com/v1/geocode/search?text=' + end.split(' ').join('%20') + '&apiKey=decf2dfaae66445faf7b1b318e8b1180')
-        //     .then(response => {
-        //         end_lon = response.data.features[0].lon;
-        //         end_lat = response.data.features[0].lat;
-                
-        //     })
-        //     .catch(error => {
-        //         console.log(error);
-        //     })
-        //     .finally(() => {
-        //         console.log(end_lon);
-        //         axios.get('https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624835290f82bf4643548cfbf31e939c3b43&start=8.681495,49.41461&end=8.687872,49.420318')
-        //             .then(response => {
-        //                 console.log(response.data.features[0].geometry.coordinates);
-                        
-        //                 io.emit('traceRoute', response.data.features[0].geometry.coordinates);
-        //             })
-        //             .catch(error => {
-        //                 console.log(error);
-        //             });
-        //     });
-        
+            .finally(() => {
+                axios.get('https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624835290f82bf4643548cfbf31e939c3b43&start=' + start_lon + ',' + start_lat + '&end=' + end_lon + ',' + end_lat)
+                    .then(response => {
+                        let res = switchLonLat(response.data.features[0].geometry.coordinates);
+                        io.emit('traceRoute', res, start_lon, start_lat);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            })
+    })
+
+    socket.on('callTracePoints', (coords) => {
+        let sep = '%2C+'
+        let req = 'https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&geofilter.polygon=';
+        for (let i = 0; i < coords.length - 1; i++) {
+            req += '(' + coords[i][1] + sep + coords[i][0] + ')' + sep;
+        }
+        req += '(' + coords[coords.length - 1][1] + sep + coords[coords.length - 1][0] + ')';
+
+        axios.get(req)
+            .then(response => {
+                io.emit('traceBornes', response.data.records);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     })
 })
 
