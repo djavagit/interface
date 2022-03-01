f7c32fda8abe66c1f6423100272761f389a4ca32
willygentet <58994972+willygentet@users.noreply.github.com>
2022-03-01 09:46:05 +0100

    bornes dones

--- server.js
+++ server.js
@@ -62,15 +62,15 @@ io.on('connection', (socket) => {
             start_lat = responses[0].data.features[0].properties.lat;
             end_lon = responses[1].data.features[0].properties.lon;
             end_lat = responses[1].data.features[0].properties.lat;
-            console.log(start_lon + ' ' + start_lat + ' ' + end_lon + ' ' + end_lat);
         })).catch(errors => {
             console.log(errors);
         })
             .finally(() => {
                 axios.get('https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf624835290f82bf4643548cfbf31e939c3b43&start=' + start_lon + ',' + start_lat + '&end=' + end_lon + ',' + end_lat)
                     .then(response => {
+                        console.log(response.data.features[0].properties.summary.distance);
                         let res = switchLonLat(response.data.features[0].geometry.coordinates);
-                        io.emit('traceRoute', res, start_lon, start_lat);
+                        io.emit('traceRoute', res, start_lon, start_lat, response.data.features[0].properties.summary.distance / 1000);
                     })
                     .catch(error => {
                         console.log(error);
@@ -78,24 +78,30 @@ io.on('connection', (socket) => {
             })
     })
 
-    socket.on('callTracePoints', (coords) => {
-        let sep = '%2C+'
-        let req = 'https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&geofilter.polygon=';
-        for (let i = 0; i < coords.length - 1; i++) {
-            req += '(' + coords[i][1] + sep + coords[i][0] + ')' + sep;
+    socket.on('callTracePoints', async (coords) => {
+        let bornes = [];
+        let sep = '%2C+';
+        let rayon = 5000;
+        for (let i = 0; i < coords.length; i++) {
+            const element = coords[i];
+            let link = 'https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&geofilter.distance=';
+            link += element[0] + sep + element[1] + sep + rayon;
+
+            await axios.get(link)
+                .then(response => {
+                    for (let i = 0; i < response.data.records.length; i++) {
+                        const element = response.data.records[i];
+                        bornes.push([element.fields.xlongitude, element.fields.ylatitude])
+                    }
+                    io.emit('traceBornes', bornes);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
-        req += '(' + coords[coords.length - 1][1] + sep + coords[coords.length - 1][0] + ')';
-
-        axios.get(req)
-            .then(response => {
-                io.emit('traceBornes', response.data.records);
-            })
-            .catch(error => {
-                console.log(error);
-            });
     })
-})
 
+})
 
 server.listen(process.env.PORT || 3000);
 
